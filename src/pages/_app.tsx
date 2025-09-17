dfebf1203d1e430060eb50f8cbe6ec2c0b6971b6
Elkin Alexis Garcia Salazar <oficialchacal@gmail.com>
2025-09-17 08:45:06 -0500

    debug: Simplificar _app.tsx para diagnosticar errores

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,8 @@
 import { AppProps } from 'next/app';
-import { ThemeProvider } from '../contexts/ThemeContext';
-import { UserProvider } from '../contexts/UserContext';
-import { RolProvider } from '../contexts/RolContext';
-import { FastUserProvider } from '../contexts/FastUserContext';
-import { ToastProvider, useToast } from '../contexts/ToastContext';
-import ToastContainer from '../components/ui/ToastContainer';
 import '../styles/globals.css';
-import '../styles/tabs-fix.css';
 
-// Componente interno para usar useToast
-function AppContent({ Component, pageProps, router }: AppProps) {
-  const { toasts, removeToast } = useToast();
-  
-  return (
-    <>
-      <Component {...pageProps} />
-      <ToastContainer toasts={toasts} onRemoveToast={removeToast} />
-    </>
-  );
-}
-
-function MyApp({ Component, pageProps, router }: AppProps) {
-  return (
-    <ThemeProvider>
-      <UserProvider>
-        <RolProvider>
-          <FastUserProvider>
-            <ToastProvider>
-              <AppContent Component={Component} pageProps={pageProps} router={router} />
-            </ToastProvider>
-          </FastUserProvider>
-        </RolProvider>
-      </UserProvider>
-    </ThemeProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
