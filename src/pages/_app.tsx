64a354a9ba029710fcf9ab640c180b7fb29cd0c2
Elkin Alexis Garcia Salazar <oficialchacal@gmail.com>
2025-09-17 08:57:51 -0500

    fix: Agregar FastUserProvider para resolver error useFastUser

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { UserProvider } from '../contexts/UserContext';
+import { FastUserProvider } from '../contexts/FastUserContext';
+import { RolProvider } from '../contexts/RolContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { ToastProvider } from '../contexts/ToastContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <UserProvider>
+        <FastUserProvider>
+          <RolProvider>
+            <ToastProvider>
+              <Component {...pageProps} />
+            </ToastProvider>
+          </RolProvider>
+        </FastUserProvider>
+      </UserProvider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
